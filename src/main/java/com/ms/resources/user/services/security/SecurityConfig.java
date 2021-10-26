1a1bf3fe1b3948138bb91e25a26053b7205c5fde
devkhchua <developer.khchua@gmail.com>
2021-10-26 18:38:50 +0800

    optimize imports

--- src/main/java/com/ms/resources/user/services/security/SecurityConfig.java
+++ src/main/java/com/ms/resources/user/services/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ms.resources.user.services.security;
 
-import com.ms.resources.user.services.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
